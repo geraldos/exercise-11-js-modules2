46ecf4801a49ae62f8f4398babb7bbf058d691c5
geraldos <sepdwijaya18@gmail.com>
2020-06-19 14:52:18 +0700

    first commit

---
+++ function.js
@@ -0,0 +1,39 @@
+function plus() {
+    let num1 = 10
+    let num2 = 10
+    let result = num1 + num2
+
+    return  result
+}
+
+function multiple() {
+    let num3 = 19
+    let num4 = 10
+    let result1 = num3 * num4
+
+    return result1
+}
+
+function divided() {
+    let num5 = 10
+    let num6 = 10
+    let result2 = num5 / num6
+
+    return result2
+}
+
+function subtraction() {
+    let num7 = 20
+    let num8 = 10
+    let result3 = num7 - num8
+
+    return result3
+}
+
+export {
+    plus,
+    multiple,
+    divided,
+    subtraction
+}
+
---
+++ index.js
@@ -0,0 +1,13 @@
+import { plus, multiple, divided, subtraction} from './function.js'
+
+console.log('10 + 10')
+console.log(plus())
+
+console.log('19 * 10')
+console.log(multiple())
+
+console.log('10 / 10')
+console.log(divided())
+
+console.log('20 - 10')
+console.log(subtraction())
